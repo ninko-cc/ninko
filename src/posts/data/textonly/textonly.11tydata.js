aa41246db544e807a7d5f9a89b5ae772420480a0
ninko-cc <264525673+ninko-cc@users.noreply.github.com>
2026-03-30 17:31:49 +0900

    feat: add dummy image for textonly posts to posts pages

--- src/posts/data/textonly/textonly.11tydata.js
+++ src/posts/data/textonly/textonly.11tydata.js
@@ -1,13 +1,15 @@
 export default {
     category: 'textonly',
     gallery: false,
+    width: 300,
+    height: 300,
+    display: 'TEXT ONLY',
     eleventyComputed: {
         rss: (data) => ({ title: data.title }),
-        thumbnail: (data) => {
+        thumbnail: () => {
             return {
                 width: 300,
                 height: 300,
-                display: 'TEXT ONLY',
             };
         },
     },
